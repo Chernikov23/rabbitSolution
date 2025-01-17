ade8e6a7af655df741c1795f714dd28ac44bb1b0
Alexander <118567820+Chernikov23@users.noreply.github.com>
2025-01-17 23:59:17 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,24 +5,40 @@ from aiogram.types import Message
 from aiogram.filters import Command
 import os
 from dotenv import load_dotenv
-from concurrent.futures import ThreadPoolExecutor
-import time
-import random
+from aiogram.exceptions import TelegramRetryAfter
+import logging
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-RABBITMQ_HOST = os.getenv('HOST')
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+RATE_LIMIT=1/30
+
+RABBITMQ_HOST = os.getenv("HOST")
 QUEUE_NAME = 'priority_queue'
-NUM_WORKERS = 5 
+
+workers = 5  
 
 class TelegramSender:
     def __init__(self, token):
         self.bot = Bot(token)
+
     async def send_message(self, chat_id, message_text):
-        await self.bot.send_message(chat_id, message_text)
+        while True:
+            try:
+                await self.bot.send_message(chat_id, message_text)
+                await asyncio.sleep(RATE_LIMIT)  
+                break 
+            except TelegramRetryAfter as e:
+                retry_seconds = getattr(e, "retry_after", 5) 
+                logger.warning(f"Flood control: retry in {retry_seconds} секунд...")
+                await asyncio.sleep(retry_seconds)
+            except Exception as e:
+                logger.error(f"error sending msg: {e}")
+                break
 
 async def send_message_worker(sender: TelegramSender, chat_id, message_text):
     await sender.send_message(chat_id, message_text)
@@ -31,7 +47,9 @@ async def send_message_worker(sender: TelegramSender, chat_id, message_text):
 async def create_queue():
     connection = await aio_pika.connect_robust(RABBITMQ_HOST)
     channel = await connection.channel()
-    queue = await channel.declare_queue('priority_queue', durable=True, arguments={'x-max-priority': 10})
+    queue = await channel.declare_queue(
+        QUEUE_NAME, durable=True, arguments={'x-max-priority': 10}
+    )
     return queue, connection
 
 
@@ -42,34 +60,40 @@ async def send_to_queue(message_text: str, chat_id: int, priority: int):
         aio_pika.Message(
             body=f'{message_text},{chat_id}'.encode(),
             priority=priority
-        ), routing_key='priority_queue'
+        ), routing_key=QUEUE_NAME
     )
     await connection.close()
+
 
 async def process_queue(queue, sender):
     async def message_handler(message: aio_pika.IncomingMessage):
         async with message.process():
-            message_data = message.body.decode('utf-8').split(',')
-            message_text = message_data[0]
-            chat_id = int(message_data[1])
-            await send_message_worker(sender, chat_id, message_text)
+            try:
+                message_data = message.body.decode('utf-8').split(',')
+                message_text = message_data[0]
+                chat_id = int(message_data[1])
+                await send_message_worker(sender, chat_id, message_text)
+            except Exception as e:
+                logger.error(f"Error: {e}")
 
     await queue.consume(message_handler)
 
+
 @dp.message(Command('send'))
 async def handle_send(message: Message):
-    for i in range(70):
+    for i in range(100):
         await send_to_queue(f"Test message {i + 1}", message.chat.id, priority=5)
-    await message.answer("Messages are being sent...")
+    await message.answer("messages're in queue and sending")
+
 
 async def main():
     queue, connection = await create_queue()
-    senders = [TelegramSender(TOKEN) for _ in range(NUM_WORKERS)]
+    senders = [TelegramSender(TOKEN) for _ in range(workers)]
     tasks = []
     for sender in senders:
         tasks.append(process_queue(queue, sender))
-    await asyncio.gather(*tasks)
-    await dp.start_polling(bot)
+    await asyncio.gather(*tasks, dp.start_polling(bot))
+
 
 if __name__ == '__main__':
     asyncio.run(main())
